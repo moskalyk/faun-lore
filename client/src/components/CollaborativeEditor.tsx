f2c8b39ae28dab985a7b1ae38cce70071a821e25
mgrok <mgrok@mgroks-MacBook-Air.local>
2021-10-07 14:14:18 -0400

    wip on faun class integrations

--- client/src/components/CollaborativeEditor.tsx
+++ client/src/components/CollaborativeEditor.tsx
@@ -1,9 +1,137 @@
 import _ from 'lodash';
 import { useEffect, useState } from 'react';
 
+// doc control
 import { getUpdatedDocFromText, initDoc, SyncClient } from '../app/sync';
 import { withErrorHandlingAsync } from './util';
 import { addEntry, getHistory, registerTextState } from 'src/_aqua/app';
+
+// user control
+import { initAfterJoin, updateOnlineStatuses } from 'src/_aqua/app';
+import { registerUserStatus } from 'src/_aqua/app';
+import { Fluence, FluencePeer, PeerIdB58 } from '@fluencelabs/fluence';
+
+
+interface User {
+    id: PeerIdB58;
+    name: string;
+    isOnline: boolean;
+}
+
+// const updateOnlineStatus = (user, onlineStatus) => {
+//     setUsers((prev) => {
+//         const result = new Map(prev);
+//         const u = result.get(user);
+//         if (u) {
+//             result.set(user, { ...u, isOnline: onlineStatus });
+//         }
+//         return result;
+//     });
+// };
+
+class Faun {
+
+    public client: SyncClient;
+    public peers: [any];
+
+    constructor(syncClient){
+        this.client = syncClient;
+
+        // register cid handler
+        registerTextState({
+            notifyTextUpdate: (changes, isAuthorized) => {
+                if (changes) {
+                    syncClient.receiveChanges(changes);
+                }
+            }
+        })
+
+        //register online status
+        this.feed()
+
+        //register userlist
+        this.numbers()
+
+        //TODO: register compute size
+
+    }
+
+    // list feed
+    async feed(){
+        try{
+            const res = await getHistory();
+                console.log('RES')
+                console.log(res)
+            for (let e of res.entries) {
+                this.client.receiveChanges(e.body);
+            }
+
+            if (this.client.getDoc() === undefined) {
+                this.client.syncDoc(initDoc());
+            }
+        }catch(e){
+            console.log('error with feed')
+            console.log(e)
+        }
+    }
+
+    // file size calculator
+    examine(cidList){
+
+    }
+
+    // push a particle
+    record(cid){
+        broadcastUpdates(cid, this.client)
+    }
+
+    // # of online peers
+    numbers(){
+        let self = this;
+
+        registerUserStatus({
+            notifyOnline: (user, onlineStatus) => {
+                console.log('ONLINE')
+                console.log(user)
+                console.log(onlineStatus)
+            },
+            notifyUserAdded: (user, isOnline) => {
+                console.log('NEW_USER')
+                console.log(user)
+                console.log(isOnline)
+                self.peers.push(user);
+
+
+                // setUsers((prev) => {
+                //     const u = user;
+                //     const result = new Map(prev);
+                //     if (result.has(u.peer_id)) {
+                //         return result;
+                //     }
+
+                //     result.set(u.peer_id, {
+                //         name: u.name,
+                //         id: u.peer_id,
+                //         isOnline: isOnline,
+                //     });
+
+                //     return result;
+                // });
+            },
+
+            notifyUserRemoved: (userLeft) => {
+                // todo
+                console.log('USER_LEFT')
+                console.log(userLeft)
+                // setUsers((prev) => {
+                //     const result = new Map(prev);
+                //     result.delete(userLeft);
+                //     return result;
+                // });
+            }
+        })
+    }
+}
 
 const broadcastUpdates = _.debounce((text: string, syncClient: SyncClient) => {
     let doc = syncClient.getDoc();
@@ -41,6 +169,7 @@ export const CollaborativeEditor = () => {
                 }
             }
         })
+        
 
         syncClient.start();
 
